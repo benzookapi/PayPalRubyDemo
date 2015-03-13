7379e4ff9fd1ecb081f4bd5d3970d7e2a94c7f6a
JO <jokksk@gmail.com>
2015-03-13 18:00:55 +0900

    #

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,11 +1,13 @@
 class TopController < ApplicationController
 
+  def main
+    #nothing
+  end
+
   def xo
-
+    p params["id"]
+    
   end
 
-  def xo2
-    render :text => "aaaaaaaaaaaaaaaaa"
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'top#main'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -54,7 +54,7 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 
-  get 'test' => 'top#xo'
+  get 'xo/:id' => 'top#xo'
 
-  get 'test2' => 'top#xo2', :as => :top
+  
 end
