4253112c4c08b0c5c7d37e6b0714dd24ef19cb00
JO <jokksk@gmail.com>
2015-03-31 16:14:14 +0900

    #

--- app/controllers/rdcheck_controller.rb
+++ app/controllers/rdcheck_controller.rb
@@ -5,7 +5,23 @@ class RdcheckController < ApplicationController
   def index
     @ug = params.has_key?(:ug) ? params[:ug] : ''
 
-    render template: 'rdcheck/index'
+
+    html =<<"HTML"
+    <html>
+    <head>
+      #{@ug}
+    </head>
+    <body>
+    <form method="POST" action="rdcheck/rd">
+      <input type="hidden" name="rd" value="https://jo-static-web.herokuapp.com/ugcheck.html">
+    <input type="submit" value="Redirect">
+    </form>
+
+    </body>
+    </html>
+HTML
+
+    render text: html
   end
 
   def rd
